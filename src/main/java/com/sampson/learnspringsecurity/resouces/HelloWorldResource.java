803f6ce57091f47eebeffd4069bcf9e3436bf38c
fsampson30 <fsampson30@gmail.com>
2024-03-05 07:44:47 -0300

    Created dummy endpoint to test CSRF.

--- src/main/java/com/sampson/learnspringsecurity/resouces/HelloWorldResource.java
+++ src/main/java/com/sampson/learnspringsecurity/resouces/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package com.sampson.learnspringsecurity;
+package com.sampson.learnspringsecurity.resouces;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
