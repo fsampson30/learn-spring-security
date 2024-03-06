4f37943e651b406db90060571b0f5c09c72a88f4
fsampson30 <fsampson30@gmail.com>
2024-03-06 07:52:08 -0300

    Stored in memory users.

--- src/main/java/com/sampson/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/sampson/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +21,12 @@ public class BasicAuthSecurityConfiguration {
             http.httpBasic();
             http.csrf(AbstractHttpConfigurer::disable);
             return http.build();
+        }
+
+        @Bean
+    public UserDetailsService userDetailsService(){
+            var user = User.withUsername("flavio").password("{noop}dummy").roles("USER").build();
+            var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
+            return new InMemoryUserDetailsManager(user,admin);
         }
 }
