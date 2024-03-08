d6572f5653b99ef0a3b0c4e38b52673cdd56ecd0
fsampson30 <fsampson30@gmail.com>
2024-03-08 07:32:05 -0300

    Added users to the H2 Database.

--- src/main/java/com/sampson/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/sampson/learnspringsecurity/security/BasicAuthSecurityConfiguration.java
@@ -2,31 +2,54 @@ package com.sampson.learnspringsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
 
-        @Bean
-        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-            http.authorizeHttpRequests(auth -> {auth.anyRequest().authenticated();});
-            http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-            http.httpBasic();
-            http.csrf(AbstractHttpConfigurer::disable);
-            return http.build();
-        }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> {
+            auth.anyRequest().authenticated();
+        });
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic();
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers().frameOptions().sameOrigin();
+        return http.build();
+    }
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
 
-        @Bean
-    public UserDetailsService userDetailsService(){
-            var user = User.withUsername("flavio").password("{noop}dummy").roles("USER").build();
-            var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
-            return new InMemoryUserDetailsManager(user,admin);
-        }
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        var user = User.withUsername("flavio").password("{noop}dummy").roles("USER").build();
+        var admin = User.withUsername("admin").password("{noop}dummy").roles("ADMIN").build();
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
+    }
 }
+
