e34e754cc123e7bbf545bcd15d1bff885f1aa0cf
fsampson30 <fsampson30@gmail.com>
2024-03-20 21:21:45 -0300

    Created endpoint to return a list of String.

--- src/main/java/com/sampson/learnspringsecurity/resouces/HelloWorldResource.java
+++ src/main/java/com/sampson/learnspringsecurity/resouces/HelloWorldResource.java
@@ -3,11 +3,18 @@ package com.sampson.learnspringsecurity.resouces;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloWorldResource {
 
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-list")
+    public List<String> helloWorldList(){
+        return List.of("Hello World1","Hello World2","Hello World3");
     }
 }
